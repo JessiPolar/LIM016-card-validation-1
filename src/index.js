7aa98e664a825ecab28ef1693928e42e30ef7c81
Jessi Polar <jessipolar1977@gmail.com>
2021-10-26 23:45:55 -0500

    formulario input listo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,84 @@
-import validator from './validator.js';
+//import validator from './validator.js';
 
-console.log(validator);
+    
+const tarjeta = document.querySelector('#tarjeta'),
+        datos = document.querySelector('#formulario-datos'),
+        formulario = document.querySelector('#formulario-tarjeta'),
+        numeroTarjeta = document.querySelector('#tarjeta .numero'),
+        nombreTarjeta = document.querySelector('#tarjeta .nombre'),
+        mesExpiracion = document.querySelector('#tarjeta .mes'),
+        yearExpiracion = document.querySelector('#tarjeta .year'),
+        cvv = document.querySelector('#tarjeta .cvv');
+
+
+//Input numero de tarjeta
+
+ formulario.inputNumero.addEventListener('keyup', (e) => {
+    let valorInput = e.target.value;
+    formulario.inputNumero.value = valorInput
+
+    //Eliminar espacios en blanco
+
+    .replace(/\s/g, '')
+
+    //Eliminar las letras
+
+    .replace(/\D/g, '')
+
+    //Espacio cada cuatro numeros
+
+    .replace(/([0-9]{4})/g, '$1 ')
+
+    //Elimina el ultimo espaciado
+
+    .trim();
+
+ });
+
+    // Input nombre de tarjeta
+
+    formulario.inputNombre.addEventListener('keyup', (e) => {
+        let valorInput = e.target.value;
+        formulario.inputNombre.value = (valorInput.toUpperCase())
+        //Eliminar numeros
+        .replace(/[0-9]/g, '');
+        
+    });
+
+   
+   
+
+
+//Seleccionar mes * generado dinamicamente.
+
+for(let i=1; i<=12; i++){
+    let opcion= document.createElement('option');
+    opcion.value= i;
+    opcion.innerText= i;
+    formulario.selectMes.appendChild(opcion);
+
+}
+
+//Seleccionar año * generado dinamicamente.
+
+const yearActual = new Date().getFullYear();
+
+for(let i= yearActual; i<=yearActual + 6; i++){
+    let opcion= document.createElement('option');
+    opcion.value= i;
+    opcion.innerText= i;
+    formulario.selectYear.appendChild(opcion);
+
+}
+
+// Input cvv
+formulario.inputcvv.addEventListener('keyup', () =>{
+    formulario.inputcvv.value = formulario.inputcvv.value
+//Eliminar los espacios
+.replace(/\s/g, '')
+// Eliminar las letras
+.replace(/\D/g, '');
+
+
+});
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,10 @@
-const validator = {
-  // ...
-};
+function validator.isValid(creditCardNumber) {
+  let array = creditCardNumber.split("");
+  let newArray = array.reverse();
+  for (let i = 0; i < newArray.length; i + 2){
 
-export default validator;
+  }
+
+}
+
+    
